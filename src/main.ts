f73bfff1bc961c41603b8689a1de615d638f6df5
张建 <475218106@qq.com>
2023-07-23 22:12:35 +0800

    提交

---
+++ src/main.ts
@@ -0,0 +1,58 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import {
+  Tabbar,
+  TabbarItem,
+  Search,
+  Icon,
+  Loading,
+  Skeleton,
+  Tabs,
+  Tab,
+  Sticky,
+  NavBar,
+  Form,
+  CellGroup,
+  Field,
+  Button,
+} from 'vant'
+
+import App from './App.vue'
+import router from './router'
+import lazyPlugin from './directives/lazyLoading'
+
+import 'vant/lib/index.css'
+
+// import './assets/main.css'
+import './assets/common.scss'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.use(Tabbar)
+app.use(TabbarItem)
+app.use(Search)
+app.use(Icon)
+app.use(Loading)
+app.use(Skeleton)
+app.use(Tabs)
+app.use(Tab)
+app.use(Sticky)
+app.use(NavBar)
+app.use(Form)
+app.use(CellGroup)
+app.use(Field)
+app.use(Button)
+app.use(lazyPlugin)
+
+const rootValue = 16
+const rootWidth = 390
+const deviceWidth = document.documentElement.clientWidth
+document.documentElement.style.fontSize = (deviceWidth * rootValue) / rootWidth + 'px'
+
+// 3-4
+// 4-2
+app.mount('#app')
